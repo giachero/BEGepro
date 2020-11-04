a0576545026a77962264307a124c4ae15f9b0c93
giachero <andrea.giachero@mib.infn.it>
2020-11-04 18:41:47 +0100

    Added .gitattributes and setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+pathmunge () {    
+    
+    ENV_VAR_NAME=$(echo $1 | tr '[:lower:]' '[:upper:]')
+    
+    eval ENV_VAR=\$$ENV_VAR_NAME    
+    if ! echo $ENV_VAR | /bin/egrep -q "(^|:)$2($|:)" ; then
+        if [ "$3" = "after" ] ; then
+	    ENV_VAR=$ENV_VAR:$2
+        else
+            ENV_VAR=$2:$ENV_VAR
+        fi
+    fi
+    eval ${ENV_VAR_NAME}=$ENV_VAR
+}
+
+
+AGPATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+pathmunge PYTHONPATH $AGPATH/src
+#pathmunge PATH $AGPATH/bin
+#pathmunge PATH $AGPATH/scripts
+
+export PYTHONPATH=$PYTHONPATH
+#export PATH=$PATH
+
+
+
