ed101ac1953fdcb52627995064ecb6752774f556
Pic143 <marcopicione98@gmail.com>
2022-12-19 18:31:43 +0100

    added part of the comments

--- begepro/dspro/bege_event.py
+++ begepro/dspro/bege_event.py
@@ -1,8 +1,30 @@
+# Class:        BEGeEvent 
+# Description:  useful class to store the pre-amplified pulses' parameters 
+# Attributes:   >> traces:      number of the events that is actually containing
+#               >> n_trace:     max number of the events that can contain
+#               >> dim_trace:   number of points describing each pulse shape     
+#               >> data:        dictionary containing all the informations about the pulses
+#                               >>trace:        pre-amplified pulse of the event
+#                               >>curr:         current signal obtained via differentiation
+#                               >>pheight:      ???
+#                               >>energy:       energy of the event
+#                               >>amplitude:    amplitude of the current
+#                               >>ae:           amplitude over energy
+#                               >>risetime:     risetime of the trace
+#                               >>zeros_2der:   number of zeros of the differentiated current
+#                               >>n_peaks_2der: number of peaks of the differentiated current
+#                               >>simm:         value of the simmetry parameter
+#                               >>labels:       label of the neural network
+#                               >>index:        enumeration of the event
+
+
+#!!! salvare solo trace con autoencoder curr non serve. Risparmiamo ram  
+
 import numpy as np
 import math as math
                     
 class BEGeEvent(object):
-    def __init__(self, n_trace, dim_trace, trace=None,curr=None, pheight=None, energy=None, amplitude=None, ae=None, risetime=None,n_peaks=None,zeros_2der=None,n_peaks_2der=None, index=None):
+    def __init__(self, n_trace, dim_trace, trace=None,curr=None, pheight=None, energy=None, amplitude=None, ae=None, risetime=None,n_peaks=None,zeros_2der=None,n_peaks_2der=None,simm=None,labels=None,index=None):
 
         self.__data={'trace'       : np.zeros([n_trace,dim_trace]).astype(np.int16)   if trace        is None else np.array(trace).astype(np.int16),
                      'curr'        : np.zeros([n_trace,dim_trace]).astype(np.float64) if curr         is None else np.array(curr).astype(np.float64), 
@@ -14,6 +36,8 @@ class BEGeEvent(object):
                      'n_peaks'     : np.zeros([n_trace]).astype(np.int16)             if n_peaks      is None else np.array(n_peaks).astype(np.int16),
                      'zeros_2der'  : np.zeros([n_trace]).astype(np.int16)             if zeros_2der   is None else np.array(zeros_2der).astype(np.int16),
                      'n_peaks_2der': np.zeros([n_trace]).astype(np.int16)             if n_peaks_2der is None else np.array(n_peaks_2der).astype(np.int16),
+                     'simm'        : np.zeros([n_trace]).astype(np.float64)           if simm         is None else np.array(simm).astype(np.float64),
+                     'labels'      : np.zeros(n_trace).astype(np.float64)             if labels       is None else np.array(labels).astype(np.double),
                      'index'       : np.zeros([n_trace]).astype(np.int16)             if index        is None else np.array(index).astype(np.int32)}
                      
         self.__traces=0
@@ -23,13 +47,19 @@ class BEGeEvent(object):
         
         return
 
+# Method:       subset
+# Description:  performs cuts on a given parameter
+# Returns:      BEGeEvent with values of the parameter "key" included between "cutmin" and "cutmax"
+#               or
+#               BEGeEvent with the events specified by the array "index"
+
     def subset(self,key,cutmin=None,cutmax=None,index=None):
         if((index is None) & (cutmin==None)):
             return
         if ((key in self.__data.keys())&((key!='trace') & (key!='curr'))): 
             if index is None:
                 if cutmax==None: cutmax=float(math.inf)  
-                index=np.where((self.get_data(key) >= cutmin) & (self.get_data(key) <= cutmax))[0]                   
+                index=np.where((self.get_data(key) >= cutmin) & (self.get_data(key) <= cutmax))[0]          
             return BEGeEvent(np.array(index).size,self.dim_trace,
                              self.get_traces()[index,:] if self.get_traces().size != 0 else None,
                              self.get_curr()[index,:] if self.get_curr().size != 0 else None,
@@ -41,14 +71,24 @@ class BEGeEvent(object):
                              self.get_n_peaks()[index],
                              self.get_zeros_2der()[index],
                              self.get_n_peaks_2der()[index],
+                             self.get_simm()[index],
+                             self.get_labels()[index],
                              self.get_indexes()[index])
     
+# Method:       update
+# Description:  updates a specified element of the dictonary "data" specified by "key"
+# Returns:      none
+
     def update(self, key, value):
         if key in self.__data.keys():       
             self.__data[key]=value
             
         return
         
+# Method:       update_index
+# Description:  updates the parameter "index" inside "data"
+# Returns:      none
+    
     def update_index(self):
         self.__data['index']=np.array(range(len(self.__data['n_peaks'])))   
         return
@@ -60,6 +100,11 @@ class BEGeEvent(object):
                 self.__traces+=1
             
         return
+        
+    def remove_zeros(self):
+        v=np.where(self.get_avse()==0)[0]
+        v=np.delete(np.arange(0,self.n_trace),v)
+        return self.subset('ae',index=v)
 
     def add_trace(self, trace):
         return self.__add_element('trace', np.array(trace).astype(np.int16))
@@ -90,6 +135,9 @@ class BEGeEvent(object):
         
     def add_n_peaks_2der(self, n_peaks_2der):
         return self.__add_element('n_peaks_2der', np.array(n_peaks_2der).astype(np.int16))
+        
+    def add_simm(self, simm):
+        return self.__add_element('simm', np.float64(simm))
        
     def get_data(self, key):
         return self.__data[key] if key in self.__data else None
@@ -127,8 +175,17 @@ class BEGeEvent(object):
     def get_n_peaks_2der(self):
         return self.get_data('n_peaks_2der')
         
+    def get_simm(self):
+        return self.get_data('simm')
+        
+    def get_labels(self):
+        return self.get_data('labels')
+        
     def get_indexes(self):
         return self.get_data('index')
+        
+    def get_n_elements(self):
+        return self.__traces
         
     def set_trace(self,trace):
         self.__data['trace']=trace
@@ -137,9 +194,17 @@ class BEGeEvent(object):
     def set_curr(self,curr):
         self.__data['curr']=curr
         return
+        
+    def set_labels(self,labels):
+        self.__data['labels']=labels
+        return
+        
+    def set_simm(self,simm):
+        self.__data['simm']=simm
+        return
 
     def get_parameters(self):
-        if all(k in self.__data.keys() for k in ['pheight', 'energy', 'amplitude', 'ae', 'risetime','n_peaks','zeros_2der','n_peaks_2der', 'index' ]):
+        if all(k in self.__data.keys() for k in ['pheight', 'energy', 'amplitude', 'ae', 'risetime','n_peaks','zeros_2der','n_peaks_2der','simm','index' ]):
             return np.transpose(np.matrix([self.__data['index'],
                                           self.__data['pheight'],
                                           self.__data['energy'],
@@ -148,7 +213,8 @@ class BEGeEvent(object):
                                           self.__data['risetime'],
                                           self.__data['n_peaks'],
                                           self.__data['zeros_2der'],
-                                          self.__data['n_peaks_2der']
+                                          self.__data['n_peaks_2der'],
+                                          self.__data['simm']
                                           ]))
         else:
             return None
@@ -166,6 +232,8 @@ class BEGeEvent(object):
                          np.hstack((self.get_n_peaks(),be.get_n_peaks())),
                          np.hstack((self.get_zeros_2der(),be.get_zeros_2der())),
                          np.hstack((self.get_n_peaks_2der(),be.get_n_peaks_2der())),
+                         np.hstack((self.get_simm(),be.get_simm())),
+                         np.hstack((self.get_labels(),be.get_labels())),
                          np.hstack((self.get_indexes(),be.get_indexes()))
                          )
    
