159ca1bc0274188c4b0fc6d74b2eb083a50ea5cc
Pic143 <marcopicione98@gmail.com>
2023-05-21 17:05:40 +0200

    modified

--- begepro/dspro/utils2.py
+++ begepro/dspro/utils2.py
@@ -11,7 +11,6 @@ class rise_time(object):
         return
             
     def compute_rt(self,trace,frequency,riseTimeLimits=(0.1,0.9)):
-
         i_m=np.where(trace==0)[0][0]
         i_min2=np.where(trace[i_m:]-riseTimeLimits[0]>=0)[0][0]+i_m
         i_max2=np.where(trace[i_m:]-riseTimeLimits[1] >=0)[0][0]+i_m
@@ -22,20 +21,20 @@ class rise_time(object):
         self.t_min=np.interp(riseTimeLimits[0],[trace[i_min1],trace[i_min2]],[i_min1,i_min2])
         self.t_max=np.interp(riseTimeLimits[1],[trace[i_max1],trace[i_max2]],[i_max1,i_max2])
 
-        res=(self.t_max-self.t_min)*frequency,tuple([i_min1,i_max2])
+        res=(self.t_max-self.t_min)*frequency, tuple([i_min1,i_max2])
                     
         return res
         
 class simm(object):
     def __init__(self):
-        self.conf=np.load('/home/marco/work/tesi/data/NewParameters/meanSSE.npy')
+        self.conf=np.load('/home/marco/work/Data/meanSSE.npy')
         o_rt=rise_time()
         rt,t=o_rt.compute_rt(self.conf,4e-9)
         
         o_der=second_derivative()
         der=o_der.compute_der(self.conf)
         der=o_der.compute_der(der)
-        self.m_conf=np.where(der==min(der))[0][0]    
+        self.m_conf=np.where(der==np.min(der))[0][0]    
         return
         
     def compute_simm(self,trace):        
@@ -61,7 +60,7 @@ class second_derivative(object):
     def __init__(self):
         return
                 
-    def compute_n_zeros(self,t,f):
+    def compute_n_zeros(self, f, t):
         l=t[1]-t[0]
         x=np.full(l,1)
         y=np.zeros(l)
@@ -90,20 +89,20 @@ class second_derivative(object):
                     
         return indexes
         
-    def compute_der(self,curr):
-        c=savgol_filter(curr,14,0)
-        f=savgol_filter(c,2,1,deriv=1)
-        f=savgol_filter(f,30,0)
-        return f
+    def compute_der(self, f):
+        smooth = savgol_filter(f, 14,0)
+        der = savgol_filter(smooth, 2, 1, deriv=1)
+        der = savgol_filter(der, 30, 0)
+        return der, smooth
         
     def compute_area(self,f):
         
         return simpson(f/max(f))
         
-    def compute_n_peaks(self,der2,t):
-        der2=abs(der2)
-        der2=der2/max(der2)   
-        indexes=find_peaks(der2,distance=10,height=0.1)[0]
+    def compute_n_peaks(self,f,t):
+        der2=abs(f)
+        der2=der2/max(f)   
+        indexes=find_peaks(f,distance=10,height=0.1)[0]
         return indexes.astype(np.int16)
              
         
