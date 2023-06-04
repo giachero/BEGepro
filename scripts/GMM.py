ac6b8d1989aa61a92867c7a27de58977498fea37
Pic143 <marcopicione98@gmail.com>
2023-06-04 19:35:46 +0200

    updated

--- scripts/GMM.py
+++ scripts/GMM.py
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split
 from sklearn import mixture
 import matplotlib.colors as clr
 
+import os
+
 
 import psutil
 import random
@@ -20,172 +22,182 @@ import pickle
 
 import IPython 
 from begepro.dspro import histfit as hf
-from begepro.dspro import utils_analysis as ua
-
-calVec = [-0.090383 + 0.20574*i for i in range(2**14+1)]
-crioconite=False
-train=False
-fit=False
+from begepro.dspro import utils_analysis as utils
 
 def main():
 
-    #OPEN FILES
+    ################################
+    # PRELIMINARY USEFUL VARIABLES #
+    ################################
 
-    print('\n')
-    print('\n')
+    # To decide the radioactive source
+    crioconite = False
+
+    # To decide if you want to compute the best cut over GMM output
+    compute = False
+
+    # To decide if fit or train the model
+    train = False
+    fit = False
+
+    # Calibration curve to calibrate crioconites (data are in adc channels)
+    a=-0.090383
+    b=0.20574
+    calVec = [a + b*i for i in range(2**14+1)]
+
+    # Setting the correct range of energies of analisis
+
     if(crioconite):
-        Emax=700
-        #Emax=2650
-        Emin=100
-        #Emin=1550
+        Emax = 700
+        Emin = 300
     else:
-        Emax=2650
-        Emin=1550
-        Emin=150
-    
-    
-    for i in range(5,45,5): #45 for crioconite
+        Emax = 2700
+        Emin = 300
+
+    #############
+    # LOAD DATA #
+    #############
+
+    # I make use of the class numpy reader that reads the signals from a numpy and returns an object bege event which 
+    # is basically a data collector
+    # If crioconite is loaded i perform the calibration
+    startFile = 40
+    endFile = 70
+    for i in range(startFile, endFile):
         if(crioconite):
-            filename='/home/marco/work/tesi/data/NewParameters/Crioconite/NoTrace/CRC01-Zebru-im22012021__'+str(i)+'.npy'
+            dir = '/home/marco/work/Data/Analized/Crioconite/'
+            filename = 'AnalizedCRC01-Zebru-im22012021_'
         else:
-            filename='/home/marco/work/tesi/data/NewParameters/228Th/NoTrace/228Th-grafico-tesi-im260421_1__'+str(i)+'.npy'
-        coll=ca.NPYreader(filename,False).get_event()
+            dir = '/home/marco/work/Data/Analized/Torio/'
+            filename = 'Analized228Th-grafico-tesi-im260421_1_'
+        coll = ca.NPYreader(dir, filename, i, include_trace = False, include_curr = False).get_event()
         if(crioconite):
-            coll.calibrate((-0.09,0.205))
-        if(i==5):
-            coll_tot=coll.subset('energy',Emin,Emax)
+            coll.calibrate((a,b))
+        if(i == startFile):
+            coll_tot = coll.subset('energy', Emin, Emax)
         else:
-            coll_tot=coll_tot+coll.subset('energy',Emin,Emax)
-            del(coll)
+            coll_tot = coll_tot+coll.subset('energy', Emin, Emax)
+        del(coll)
             
         print('opened '+str(i)+' , Ram: '+str(psutil.virtual_memory()[2]))
         
-    coll_tot=coll_tot.subset('energy',Emin,Emax)
-        
+    # This istruction is necessary in order to correctly set up data. In the data collector there are some null events to to
+    # a very basic prefiltering that i implemented (i discard some noisy data)
+    coll_tot = coll_tot.subset('energy',Emin,Emax).remove_zeros()
+
+    # Get the parameters    
     matrix=coll_tot.get_parameters()
-      
+
+    # Spectrum in order to see the loaded data  
     plt.figure()
     c_or, e_or, p_or = plt.hist(coll_tot.get_energies(), bins=calVec, histtype='step', label='Spettro originario')
     plt.semilogy()
     plt.xlim(Emin,Emax)
+    plt.title('Spectrum of ' + ('Cryoconite' if crioconite else '228Th'))
+    plt.xlabel('Energy [keV]')
+    plt.ylabel('Counts')
     plt.show()
     
-    
-    #DATA STRUCTURE
+    # Before applying clustering i select the desired variables and i scale them.
+    # Risetime, simm are continuous while n_peaks_2der is not. Applying the scaler in such a manner 
+    # is not properly correct. 
+    ##### TO BE FIXED BEFORE ARTICLE #####
+    # Since has to be changed may use scaler from sklearn
 
-    df=pd.DataFrame(matrix,columns=['index']+list(coll_tot.get_dict().keys())[2:-2])
-    df['index']=np.arange(0,len(df['index']))
-    df_par=df[['risetime','simm','area','n_peaks_2der','zeros_2der','ae']]
-    df_par=(df_par-df_par.mean())/df_par.std()
+    df=pd.DataFrame(matrix, columns=['index'] + list(coll_tot.get_dict().keys())[2:-2])
+    df['index'] = np.arange(0, len(df['index']))
+    df_par = df[['simm','n_peaks_2der','risetime']]
+    df_par = (df_par-df_par.mean()) / df_par.std()
     print(df_par)
     del(df)
-    
-    #NAMING STUFF
-    
-    clusters=2
-    n_data=0
-    name='/home/marco/work/tesi/data/GMM_'
-    if(crioconite):
-        name=name+'Crioconite'
-    else:
-        name=name+'228Th'
-    name=name+'_c'+str(clusters)+'_e'+str(Emin)+'_'
-    n_data='_'+str(n_data)+'.npy'
-    
-    #GMM
-    X=np.array(df_par)
-    if(train):
-        model=mixture.GaussianMixture(n_components=clusters, covariance_type="spherical")
-        X=np.array(df_par)
-        model.fit(X)
+    X = np.array(df_par)
 
-        labels=model.predict_proba(X)
-        print(labels)
-        #centers=model.centers
-        
-        #np.save(name+'centres'+n_data,centers)
-        np.save(name+'labelsTot'+n_data,labels)
-        
-        file_name = name+'model'+n_data[0:(len(n_data)-3)]+'pkl'
-        
-        with open(file_name,'wb') as file:
-            pickle.dump(model,file)
+    ###########################
+    # GAUSSIAN MIXTURE MODELS #
+    ###########################
     
+    # Useful variables
+
+    # Useful variable to try fitting different times and change the name of the model
+    n_data = str(startFile) + '_' + str(endFile - 1)
+
+    #Construction of the name of the file
+    filename = '/home/marco/work/Data/Labels/GMM_'
+    if(crioconite):
+        filename = filename + 'Crioconite'
+    else:
+        filename = filename + '228Th'
+
+    filename = filename + '_E' + str(Emin) + '_' + str(Emax)
+    filename = filename + 'Data_' + n_data + '.npy'
+
+    # Useful paths
+    labels_filename = filename
+    model_filename = '/home/marco/work/Data/Models/GMM_model_' 
+    model_filename += 'Cr_' if crioconite else 'Th'
+    model_filename += '.pkl'
     
-    #SET LABELS
-    if(fit):
-        if(crioconite):
-            file_name ='/home/marco/work/tesi/data/GMM_Crioconite_c2_e1550_model_0.pkl'
-        else:
-            file_name ='/home/marco/work/tesi/data/GMM_228Th_c2_e1550_model_0.pkl'
+    # Construction of the model
+    
+    if os.path.isfile(labels_filename):
+        # The labels are alredy present
+        print('Found labels. Loading them ...')
+
+        labels = np.load(labels_filename)
+
+        print('Loaded')
         
-        X=np.array(df_par)
-        filehandler = open(file_name, 'rb') 
+    elif os.path.isfile(model_filename):
+        # The model exists
+        print('Found model. Loading it ...')
+        print(model_filename)
+
+        # Open
+        filehandler = open(model_filename, 'rb') 
         model = pickle.load(filehandler)
-        labels=model.predict_proba(X)
-    else:    
-        labels=np.load(name+'labelsTot'+n_data)
+        print('Loaded')
+
+        # Predict classes
+        print('Predicting classes ...')
+        labels = model.predict_proba(X)
+        print('Done')
+
+        # Save them
+        np.save(labels_filename, labels)  
+        print('Saved')
+
+    else: 
+        # Train the model
+        print('Fitting data ...')
+        model = mixture.GaussianMixture(n_components = 2, covariance_type = "full")
+        model.fit(X)
+        labels = model.predict_proba(X)
+        print('Done')
+
+        # Save model and labels
+        np.save(labels_filename, labels)  
+
+        with open(model_filename,'wb') as file:
+            pickle.dump(model,file)
+
+        print('Saved')
     
-    coll_tot.set_labels(labels)
-    print('\n')
-    print(labels)
-    
-    #HIST OF LABELS VS AE
-    
-    plt.figure()
-    for i in range(0,clusters):
-        coll_tot.set_labels(labels[:,i])
-        c_or, e_or, p_or = plt.hist(coll_tot.subset('labels',0.8,1).get_avse(),bins=np.linspace(0,0.05,500),histtype='step',density=True, alpha=0.7,label='Label '+str(i))
-    
-    plt.xlabel('ae')
-    plt.ylabel('Conteggi normalizzati')
-    plt.legend()
-    plt.show()
-    j=int(input())
-    coll_tot.set_labels(1-labels[:,j])
-    
-    #HIST OF LABELS
+    # Set labels
+    coll_tot.set_labels(labels[:, 0])
+    print(coll_tot.get_labels())
+
+    # Show them
     plt.figure()
     plt.hist(coll_tot.get_labels(),color='b',alpha=1,bins=np.arange(0,1.1,0.005),density=True)
     plt.xlabel('Label')
-    plt.ylabel('Conteggi normalizzati')
+    plt.ylabel('Normalized counts')
     plt.show()
-    """  
-    #HIST 2D
-    
-    fig,axs=plt.subplots(3,figsize=(20,20))
-    for i in range(0,clusters):
-        r=axs[i].hist2d(coll_tot.get_avse(),labels[:,i], bins=(np.linspace(0.01,0.03,600),np.linspace(0,1,500)), cmap=plt.cm.turbo, norm=clr.LogNorm())
-        axs[i].set(xlabel='ae',ylabel='label '+str(i))
-    plt.show()
-    
-    
-    #SCATTER GRID PLOT
-    
-    n=len(df_par.keys())
-    maxP=50000
-    keys=df_par.keys()
-    fig,axs=plt.subplots(n,n,figsize=(40,20))
-    for i in range(0,n):
-        for j in range(0,n):
-            r=axs[i][j].hist2d(df_par[keys[i]][0:maxP],df_par[keys[j]][0:maxP], bins=(np.linspace(-2,2,200),np.linspace(-2,2,200)), cmap=plt.cm.turbo, norm=clr.LogNorm())
-            axs[i][j].set(xlabel=keys[i],ylabel=keys[j])
-            
-    fig.subplots_adjust(hspace=0.5)
-    fig.subplots_adjust(wspace=0.3)
-    plt.show()
-    """
-    """      
-    #CMvsAE EXPERIMENTAL
-    labels2=np.array(1-labels[:,2])
-    labels0=np.array(labels[:,0])
-    #z=np.zeros((1,len(labels0)))
-    
-    labels=np.where((labels0>0.6) & (labels0<1),labels0,labels2)
-    print(labels)
-    coll_tot.set_labels(labels)
-    """
-    
+
+    ############
+    # BEST CUT #
+    ############
+
+    # Some parameters for 228Th and cryoconite
     if(crioconite):
         #Crioconite
         comparison_energy=(605,615)
@@ -196,18 +208,120 @@ def main():
         comparison_energy=(2100,2110)
         peak={  'double_escape' :(1590,1600),
                 'peakBi'        :(1620,1625),         
-                'peakTl'        :(2605,2620)}
+                'full-energy'   :(2605,2620)}
         compton=(1850,2000)
+
+    obj = utils.analysis(calVec)
     
-    #CMvsAE
-    obj=ua.analysis(calVec)
-    k=np.linspace(0.1,1,10)
-    obj.AIvsAE(coll_tot,peak,comparison_energy,compton,k)
+    if compute:
+        print('Start computing best cut')
+        peak_compton_list = []
+        cuts = []
+        for i, cut in enumerate(np.linspace(0.01, 1, 100)):
+            peak_compton_list.append(obj.peak_compton2(coll_tot.subset('labels', 0, cut), peak['full-energy'], compton)[0])
+            cuts.append(i)
+            if i//10 == 0: print((i+1) * 10,'%')
+
+        fig = plt.figure()
+        plt.scatter([round(elem, 2) for elem in cuts], peak_compton_list)
+        plt.xticks(rotation = 90)
+        plt.xlabel('Cut')
+        plt.ylabel('Peak / Compton')
+        plt.show()
+
+        cut = np.linspace(0.01, 1, 100)[np.argmax(peak_compton_list)]
+        print('Computed ', cut)
+    else:
+        cut = 0.1
+
+    # I want to save the MSE as class 0. So I cycle until done
+    while(True):
+        # Invert labels
+        coll_tot.set_labels(1 - coll_tot.get_labels())
+        plt.figure()
+        c_or, e_or, p_or = plt.hist(coll_tot.get_energies(), bins=calVec, histtype='step', label='original')
+        c_CNN, e_CNN, p_CNN = plt.hist(coll_tot.subset('labels', 0, cut).get_energies(), bins=calVec, histtype='step', label='Cut GMM')
+        plt.xlim(Emin, Emax)
+        plt.legend()
+        plt.semilogy()
+        plt.show()
+        if input("Are MSE class 0? "): break
+    
+    # Save the energies
+    filename = '/home/marco/work/Data/SavedEvents/GMM_energies.npy'
+    np.save(filename, coll_tot.subset('labels', 0, cut).get_energies())
+
+
+    ####################
+    # IGNORE THIS PART #
+    ####################
+        
+    # #HIST OF LABELS VS AE
+    
+    # plt.figure()
+    # for i in range(0,clusters):
+    #     coll_tot.set_labels(labels[:,i])
+    #     c_or, e_or, p_or = plt.hist(coll_tot.subset('labels',0.8,1).get_avse(),bins=np.linspace(0,0.05,500),histtype='step',density=True, alpha=0.7,label='Label '+str(i))
+    
+    # plt.xlabel('ae')
+    # plt.ylabel('Conteggi normalizzati')
+    # plt.legend()
+    # plt.show()
+    # j=int(input())
+    # coll_tot.set_labels(1-labels[:,j])
+    
+    # #HIST OF LABELS
+    # plt.figure()
+    # plt.hist(coll_tot.get_labels(),color='b',alpha=1,bins=np.arange(0,1.1,0.005),density=True)
+    # plt.xlabel('Label')
+    # plt.ylabel('Conteggi normalizzati')
+    # plt.show()
+ 
+    # #HIST 2D
+    
+    # fig,axs=plt.subplots(3,figsize=(20,20))
+    # for i in range(0,clusters):
+    #     r=axs[i].hist2d(coll_tot.get_avse(),labels[:,i], bins=(np.linspace(0.01,0.03,600),np.linspace(0,1,500)), cmap=plt.cm.turbo, norm=clr.LogNorm())
+    #     axs[i].set(xlabel='ae',ylabel='label '+str(i))
+    # plt.show()
+    
+    
+    # #SCATTER GRID PLOT
+    
+    # n=len(df_par.keys())
+    # maxP=50000
+    # keys=df_par.keys()
+    # fig,axs=plt.subplots(n,n,figsize=(40,20))
+    # for i in range(0,n):
+    #     for j in range(0,n):
+    #         r=axs[i][j].hist2d(df_par[keys[i]][0:maxP],df_par[keys[j]][0:maxP], bins=(np.linspace(-2,2,200),np.linspace(-2,2,200)), cmap=plt.cm.turbo, norm=clr.LogNorm())
+    #         axs[i][j].set(xlabel=keys[i],ylabel=keys[j])
+            
+    # fig.subplots_adjust(hspace=0.5)
+    # fig.subplots_adjust(wspace=0.3)
+    # plt.show()
+   
+    # #CMvsAE EXPERIMENTAL
+    # labels2=np.array(1-labels[:,2])
+    # labels0=np.array(labels[:,0])
+    # #z=np.zeros((1,len(labels0)))
+    
+    # labels=np.where((labels0>0.6) & (labels0<1),labels0,labels2)
+    # print(labels)
+    # coll_tot.set_labels(labels)
+
+    
+    
+    
+    # #CMvsAE
+    # obj=ua.analysis(calVec)
+    # k=np.linspace(0.1,1,10)
+    # obj.AIvsAE(coll_tot,peak,comparison_energy,compton,k)
         
     
-    #SPECTRUM
-    cutAI=float(input())
-    obj.histogram(coll_tot,cutAI,comparison_energy,(Emin,Emax))
+    # #SPECTRUM
+    # cutAI=float(input())
+    # obj.histogram(coll_tot,cutAI,comparison_energy,(Emin,Emax))
     
     
     return
